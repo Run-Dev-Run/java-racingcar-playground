aaf7f35966e52a25c321f3bb684da7aade2757b7
devrunner21 <devrunner21@gmail.com>
2022-03-15 04:13:40 +0900

    refactor: 시도 횟수 저장을 위해 만들었던 Try Class를 TryCount Class로 변경하라

--- src/main/java/TryCount.java
+++ src/main/java/TryCount.java
@@ -1,17 +1,22 @@
-public class Try {
-
+public class TryCount {
     public static final String TRY_COUNT_INVALID_MESSAGE = "Try 횟수는 1 이상만 입력이 가능합니다.";
+    public static final int MIN_TRY_COUNT = 0;
+
     private final int count;
 
-    public Try(int count) {
+    public TryCount(int count) {
         validate(count);
         this.count = count;
     }
 
     private void validate(int count) {
-        if (count <= 0) {
+        if (count <= MIN_TRY_COUNT) {
             throw new IllegalArgumentException(TRY_COUNT_INVALID_MESSAGE);
         }
     }
 
+    public boolean isOverThan(int currentTryCount) {
+        return count > currentTryCount;
+    }
+    
 }
---
+++ src/test/java/TryCountTest.java
@@ -0,0 +1,29 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("TryCount 단위 테스트")
+class TryCountTest {
+
+    public static final int MORE_THAN_ZERO_NUMBER = 0;
+    public static final int LESS_THAN_ZERO_NUMBER = 1;
+
+    @Test
+    @DisplayName("TryCount는 0 이하를 인자로 전달할 경우 예외를 던집니다.")
+    void test_construct_TryCount_validate_less_than_zero() {
+        assertThatThrownBy(() -> new TryCount(MORE_THAN_ZERO_NUMBER))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(TryCount.TRY_COUNT_INVALID_MESSAGE);
+    }
+
+    @Test
+    @DisplayName("TryCount는 1 이상을 인자로 전달할 경우 인스턴스를 생성합니다.")
+    void test_construct_TryCount_validate_more_than_zero() {
+        TryCount tryCount = new TryCount(LESS_THAN_ZERO_NUMBER);
+
+        assertThat(tryCount).isNotNull();
+    }
+
+}
